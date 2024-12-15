31b1e61852387fa84b1027e12b5417813c223e4b
Bohdan Haponiuk <jlomka54@gmail.com>
2024-12-15 22:21:36 +0400

    add await

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -37,7 +37,7 @@ export const getContactsByIdControler = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
-  const contact = createContact(req.body);
+  const contact = await createContact(req.body);
   res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,

883f908f6cb43421575f550d12ab94e229f446a6
Bohdan Haponiuk <jlomka54@gmail.com>
2024-12-24 09:02:04 +0400

    delete requaried for email

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ import { contactsType } from '../constants/contacts.js';
 export const contactsAddSchema = Joi.object({
   name: Joi.string().required().min(3).max(20),
   phoneNumber: Joi.string().required(),
-  email: Joi.string().email().required().min(3).max(20),
+  email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...contactsType),
 });
